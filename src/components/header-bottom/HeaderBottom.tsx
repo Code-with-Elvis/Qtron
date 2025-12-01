cc37836c5f4599e8c1f93833c0cb80190620a10f
Elvis Okumu <okothelvis137@gmail.com>
2025-12-01 11:29:31 +0300

    Fix: Enhance responsiveness.

--- src/components/header-bottom/HeaderBottom.tsx
+++ src/components/header-bottom/HeaderBottom.tsx
@@ -14,12 +14,12 @@ const HeaderBottom = () => {
     <header>
       <div className="qtron-container bg-secondary flex items-center gap-4 ">
         <CategoriesBtn />
-        <nav className="flex gap-2">
+        <nav className="flex gap-2 overflow-x-auto flex-1 min-w-0">
           {links.map((link) => (
             <Link
               key={link.name}
               href={link.href}
-              className="h-9 border-transparent hover:border-white border px-1.5 rounded flex items-center transition-all gap-0.5 duration-100 ease-in-out text-muted-foreground hover:text-foreground text-sm"
+              className="h-9 border-transparent hover:border-white border px-1.5 rounded flex items-center transition-all gap-0.5 duration-100 ease-in-out text-muted-foreground hover:text-foreground text-sm whitespace-nowrap"
             >
               {link.name}
             </Link>
