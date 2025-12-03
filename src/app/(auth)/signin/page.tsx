a1c9b8e65e2acd8aaf7b6abfcc927a32128516ee
Elvis Okumu <okothelvis137@gmail.com>
2025-12-03 16:12:04 +0300

    Feat: Add logo and link to homepage in SignIn and SignUp pages.

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,18 @@
+import Logo from "@/assets/Logo";
 import LoginForm from "@/components/auth/LoginForm";
+import Link from "next/link";
 
 const SignIn = () => {
   return (
     <section className="py-10">
       <div className="qtron-container">
+        <Link
+          href="/"
+          className="h-11 w-fit mx-auto mb-2 border-transparent hover:border-border border px-1.5 rounded flex items-center gap-1 text-xl font-semibold transition-all duration-100 ease-in-out"
+        >
+          <Logo className="size-8 text-primary" />
+          <span>Qtron</span>
+        </Link>
         <LoginForm />
       </div>
     </section>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/assets/Logo";
 import RegisterForm from "@/components/auth/RegisterForm";
 import Link from "next/link";
 
@@ -5,6 +6,13 @@ const SignUp = () => {
   return (
     <section className="py-10">
       <div className="qtron-container">
+        <Link
+          href="/"
+          className="h-11 w-fit mx-auto mb-2 border-transparent hover:border-border border px-1.5 rounded flex items-center gap-1 text-xl font-semibold transition-all duration-100 ease-in-out"
+        >
+          <Logo className="size-8 text-primary" />
+          <span>Qtron</span>
+        </Link>
         <RegisterForm />
         <footer className="text-center text-sm leading-relaxed text-muted-foreground mt-8 max-w-[400px] mx-auto">
           By continuing, you agree to Qtron&#39;{" "}
