6e373dc49867b1ada7cb6d2c52d3de268d8330a7
Elvis Okumu <okothelvis137@gmail.com>
2025-12-13 09:14:02 +0300

    Refactor: Simplify structure of DangerZone component by removing unnecessary section wrapper.

--- src/components/profile/DangerZone.tsx
+++ src/components/profile/DangerZone.tsx
@@ -3,30 +3,26 @@ import DeleteAccountBtn from "./DeleteAccountBtn";
 
 const DangerZone = () => {
   return (
-    <section className="py-12">
-      <div className="qtron-container">
-        <h2 className="text-xl font-bold">Deletion & Deactivation</h2>
+    <div className="py-12">
+      <h2 className="text-xl font-bold">Deletion & Deactivation</h2>
 
-        <article className="mt-6 border border-yellow-600/40 rounded p-6 bg-yellow-600/10">
-          <h4 className="text-lg font-semibold mb-1">Deactivate account</h4>
-          <p className="mb-3">
-            Temporarily hide your profile and remove your presence from the
-            platform.
-          </p>
-          <DeactivationBtn />
-        </article>
-        <article className="mt-6 border border-red-600/40 rounded p-6 bg-red-600/10">
-          <h4 className="text-lg font-semibold mb-1">
-            Delete data and account
-          </h4>
-          <p className="mb-3">
-            Permanently delete your account and all associated data. This action
-            is irreversible.
-          </p>
-          <DeleteAccountBtn />
-        </article>
-      </div>
-    </section>
+      <article className="mt-6 border border-yellow-600/40 rounded p-6 bg-yellow-600/10">
+        <h4 className="text-lg font-semibold mb-1">Deactivate account</h4>
+        <p className="mb-3">
+          Temporarily hide your profile and remove your presence from the
+          platform.
+        </p>
+        <DeactivationBtn />
+      </article>
+      <article className="mt-6 border border-red-600/40 rounded p-6 bg-red-600/10">
+        <h4 className="text-lg font-semibold mb-1">Delete data and account</h4>
+        <p className="mb-3">
+          Permanently delete your account and all associated data. This action
+          is irreversible.
+        </p>
+        <DeleteAccountBtn />
+      </article>
+    </div>
   );
 };
 export default DangerZone;
